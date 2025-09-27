cec2ac90bdb0c1c9ddf32efa94191c2012730b8c
sergeev.toll <tolya@edchess.io>
2025-09-27 04:50:09 +0300

    fix: status bar

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'pay-hub',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    StatusBar: {
+      overlaysWebView: false,
+    },
+  },
 };
 
 export default config;
